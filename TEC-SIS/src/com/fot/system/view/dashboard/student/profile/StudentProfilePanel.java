a1f89297a9cf7137999bc0b58cc3e2682ab8a37e
shanuka-saranga <shanukasaranga974@gmail.com>
2026-04-24 21:52:19 +0530

    Shorten student profile panel constants

--- TEC-SIS/src/com/fot/system/view/dashboard/student/profile/StudentProfilePanel.java
+++ TEC-SIS/src/com/fot/system/view/dashboard/student/profile/StudentProfilePanel.java
@@ -4,6 +4,10 @@ import com.fot.system.model.User;
 import com.fot.system.view.dashboard.shared.UserProfilePanel;
 
 public class StudentProfilePanel extends UserProfilePanel {
+    private static final String SUBTITLE_TEXT =
+            "Review your student profile. Students can update only contact details and profile picture.";
+    private static final String ACCESS_HINT =
+            "Can update only contact details and profile picture of their profile.";
 
     public StudentProfilePanel(User user) {
         super(user);
@@ -21,16 +25,16 @@ public class StudentProfilePanel extends UserProfilePanel {
 
     @Override
     protected String getSubtitleText() {
-        return "Review your student profile. Students can update only contact details and profile picture.";
+        return SUBTITLE_TEXT;
     }
 
     @Override
     protected String getViewAccessHint() {
-        return "Can update only contact details and profile picture of their profile.";
+        return ACCESS_HINT;
     }
 
     @Override
     protected String getEditAccessHint() {
-        return "Can update only contact details and profile picture of their profile.";
+        return ACCESS_HINT;
     }
 }
